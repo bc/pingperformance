a4bddb8a254a170f8a7985f957a3bf0308e1aea9
Brian Cohn <brian.cohn@usc.edu>
2020-01-14 14:47:21 -0800

    operational to separate log files

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # pingperformance
 script to get connectivity/performance across nodes in network via ping. unix based
+
+Make sure the ip_list.txt has an IP per row you want to monitor
+
+```bash
+python app.py
+```
+
+Control+C to end
+
---
+++ app.py
@@ -0,0 +1,18 @@
+import os
+
+def compose_ping_command(ip,target_filepath):
+	date_formatting = "+%s"
+	return "ping %s | while read line; do echo `date %s` - $line; done >> %s"%(ip,date_formatting,target_filepath)
+
+def main():
+	ip_list = [line.rstrip('\n') for line in open("ip_list.txt")]
+	commands = [compose_ping_command(ip,"out_%s.txt"%(ip)) for ip in ip_list]
+	res = "&".join(commands)
+	print("Process init for IPs")
+	[print(str(ip)) for ip in ip_list]
+	os.system(res)
+
+# unixtime date +%s
+
+if __name__ == '__main__':
+	main()
